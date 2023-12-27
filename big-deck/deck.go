09eaff0443f5fc9fe5ce3bdbe7226bc806ee3a8d
Dillon Rice <drice89@gmail.com>
2023-12-27 11:33:53 -0800

    add new deck

--- big-deck/deck.go
+++ big-deck/deck.go
@@ -1,7 +1,43 @@
 package main
 
+import "fmt"
+
 // create a new type of deck
 
-// slive of strings
+// slice of type string. Note that append is still working on this
 type deck []string
 
+func newDeck() deck {
+	// creates new variable cards of type deck (a slice of strings)
+	cards := deck{}
+	// create suits
+	cardSuits := [4]string{"Hearts", "Diamonds", "Spades", "Clubs"}
+	cardValues := [13]string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four", 
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine", 
+		"Ten",
+		"Jack",
+		"Queen",
+		"King",
+	}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) showDeck() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- big-deck/main.go
+++ big-deck/main.go
@@ -1,23 +1,8 @@
 package main
 
-// auto imported... coool!
-import "fmt"
-
 func main() {
 
 	// deck defined in deck.go - does not need to be imported because it is in the same package
-	cards := deck{
-		newCard(),
-		"Ace of Diamonds",
-	}
-	cards = append(cards, "Six of spades")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.showDeck()
 }
